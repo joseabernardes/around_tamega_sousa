7bc8e46fc374dbf308e825e744898756d1871fdc
José Paulo Bernardes <8150148@estg.ipp.pt>
2017-12-27 09:57:32 +0000

    Melhoria na galeria de imagens

--- app/src/main/java/estg/ipp/pt/aroundtmegaesousa/AddPointActivity.java
+++ app/src/main/java/estg/ipp/pt/aroundtmegaesousa/AddPointActivity.java
@@ -1,54 +1,59 @@
 package estg.ipp.pt.aroundtmegaesousa;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.graphics.Point;
-import android.graphics.Rect;
+
+import android.content.Intent;
+import android.graphics.Matrix;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
-import com.jsibbold.zoomage.ZoomageView;
+import com.github.chrisbanes.photoview.PhotoView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddPointActivity extends AppCompatActivity {
-    /**
-     * Images
-     */
-    private Animator mCurrentAnimator;
-    private int mShortAnimationDuration;
+
     private ImageView thirdImg;
     private ImageView firstImg;
     private ImageView secondImg;
     private ImageView fourthImg;
     private ImageView fithImg;
     private boolean imageOpen;
-    private ZoomageView expandedImageView;
-    private View selectedImage;
-    private Rect startBounds;
-    private float startScaleFinal;
+    private PhotoView expandedImageView;
+    private Menu menu;
+    private Toolbar toolbar;
+    private int[] images;
+    private List<Integer> imagesList;
+    private ArrayList<File> photos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_point);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        myToolbar.setTitle(R.string.add_point);
-        setSupportActionBar(myToolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.add_point);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        photos = new ArrayList<File>();
 
 
+        imagesList = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            imagesList.add(i, R.drawable.ic_add);
+        }
+        imagesList.add(0, R.drawable.img_1);
+        imagesList.add(1, R.drawable.img_2);
         /**
          * gallery
          */
         expandedImageView = findViewById(R.id.expanded_image);
-        startBounds = new Rect();
         imageOpen = false;
         firstImg = findViewById(R.id.first_img);
         secondImg = findViewById(R.id.second_img);
@@ -59,31 +64,32 @@ public class AddPointActivity extends AppCompatActivity {
         firstImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                zoomImageFromThumb(firstImg, R.drawable.img_1);
+                onClickImage(firstImg, imagesList.get(0));
+
             }
         });
         secondImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                zoomImageFromThumb(secondImg, R.drawable.img_2);
+                onClickImage(secondImg, imagesList.get(1));
             }
         });
         thirdImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                zoomImageFromThumb(thirdImg, R.drawable.img_3);
+                onClickImage(thirdImg, imagesList.get(2));
             }
         });
         fourthImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                zoomImageFromThumb(fourthImg, R.drawable.img_4);
+                onClickImage(fourthImg, imagesList.get(3));
             }
         });
         fithImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                zoomImageFromThumb(fithImg, R.drawable.img_5);
+                onClickImage(fithImg, imagesList.get(4));
             }
         });
     }
@@ -91,6 +97,7 @@ public class AddPointActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -111,186 +118,37 @@ public class AddPointActivity extends AppCompatActivity {
     }
 
 
-    private void zoomImageFromThumb(final View thumbView, int imageResId) {
-        selectedImage = thumbView;
-        // If there's an animation in progress, cancel it
-        // immediately and proceed with this one.
-        if (mCurrentAnimator != null) {
-            mCurrentAnimator.cancel();
-        }
-
-        // Load the high-resolution "zoomed-in" image.
-
-        expandedImageView.setImageResource(imageResId);
-
-        // Calculate the starting and ending bounds for the zoomed-in image.
-        // This step involves lots of math. Yay, math.
-        final Rect finalBounds = new Rect();
-        final Point globalOffset = new Point();
-
-        // The start bounds are the global visible rectangle of the thumbnail,
-        // and the final bounds are the global visible rectangle of the container
-        // view. Also set the container view's offset as the origin for the
-        // bounds, since that's the origin for the positioning animation
-        // properties (X, Y).
-        selectedImage.getGlobalVisibleRect(startBounds);
-        findViewById(R.id.container)
-                .getGlobalVisibleRect(finalBounds, globalOffset);
-        startBounds.offset(-globalOffset.x, -globalOffset.y);
-        finalBounds.offset(-globalOffset.x, -globalOffset.y);
-
-        // Adjust the start bounds to be the same aspect ratio as the final
-        // bounds using the "center crop" technique. This prevents undesirable
-        // stretching during the animation. Also calculate the start scaling
-        // factor (the end scaling factor is always 1.0).
-        float startScale;
-        if ((float) finalBounds.width() / finalBounds.height()
-                > (float) startBounds.width() / startBounds.height()) {
-            // Extend start bounds horizontally
-            startScale = (float) startBounds.height() / finalBounds.height();
-            float startWidth = startScale * finalBounds.width();
-            float deltaWidth = (startWidth - startBounds.width()) / 2;
-            startBounds.left -= deltaWidth;
-            startBounds.right += deltaWidth;
+    private void onClickImage(final View thumbView, int imageResId) {
+        if (imageResId != R.drawable.ic_add) { //se já tiver imagem
+            expandedImageView.setImageResource(imageResId);
+            expandedImageView.setVisibility(View.VISIBLE);
+            updateMenuItem("Eliminar", R.drawable.ic_cancel);
+            toolbar.setTitle("Imagem " + imageResId);
+            imageOpen = true;
         } else {
-            // Extend start bounds vertically
-            startScale = (float) startBounds.width() / finalBounds.width();
-            float startHeight = startScale * finalBounds.height();
-            float deltaHeight = (startHeight - startBounds.height()) / 2;
-            startBounds.top -= deltaHeight;
-            startBounds.bottom += deltaHeight;
+
+            //adicionar imagem
         }
 
-        // Hide the thumbnail and show the zoomed-in view. When the animation
-        // begins, it will position the zoomed-in view in the place of the
-        // thumbnail.
-/*        selectedImage.setAlpha(0f);*/
-
-        expandedImageView.setVisibility(View.VISIBLE);
-
-        // Set the pivot point for SCALE_X and SCALE_Y transformations
-        // to the top-left corner of the zoomed-in view (the default
-        // is the center of the view).
-        expandedImageView.setPivotX(0f);
-        expandedImageView.setPivotY(0f);
-
-        // Construct and run the parallel animation of the four translation and
-        // scale properties (X, Y, SCALE_X, and SCALE_Y).
-        AnimatorSet set = new AnimatorSet();
-        set
-                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
-                        startBounds.left, finalBounds.left))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
-                        startBounds.top, finalBounds.top))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
-                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
-                View.SCALE_Y, startScale, 1f));
-        set.setDuration(mShortAnimationDuration);
-        set.setInterpolator(new DecelerateInterpolator());
-        set.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                mCurrentAnimator = null;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                mCurrentAnimator = null;
-            }
-        });
-        set.start();
-        mCurrentAnimator = set;
-
-        // Upon clicking the zoomed-in image, it should zoom back down
-        // to the original bounds and show the thumbnail instead of
-        // the expanded image.
-        startScaleFinal = startScale;
-    /*    expandedImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mCurrentAnimator != null) {
-                    mCurrentAnimator.cancel();
-                }
-
-                // Animate the four positioning/sizing properties in parallel,
-                // back to their original values.
-                AnimatorSet set = new AnimatorSet();
-                set.play(ObjectAnimator
-                        .ofFloat(expandedImageView, View.X, startBounds.left))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.Y, startBounds.top))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_X, startScaleFinal))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_Y, startScaleFinal));
-                set.setDuration(mShortAnimationDuration);
-                set.setInterpolator(new DecelerateInterpolator());
-                set.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        selectedImage.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        selectedImage.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-                });
-                set.start();
-                mCurrentAnimator = set;
-            }
-        });*/
-        imageOpen = true;
     }
 
 
     private void closeImage() {
-        if (mCurrentAnimator != null) {
-            mCurrentAnimator.cancel();
-        }
-        // Animate the four positioning/sizing properties in parallel,
-        // back to their original values.
-        AnimatorSet set = new AnimatorSet();
-        set.play(ObjectAnimator
-                .ofFloat(expandedImageView, View.X, startBounds.left))
-                .with(ObjectAnimator
-                        .ofFloat(expandedImageView,
-                                View.Y, startBounds.top))
-                .with(ObjectAnimator
-                        .ofFloat(expandedImageView,
-                                View.SCALE_X, startScaleFinal))
-                .with(ObjectAnimator
-                        .ofFloat(expandedImageView,
-                                View.SCALE_Y, startScaleFinal));
-        set.setDuration(mShortAnimationDuration);
-        set.setInterpolator(new DecelerateInterpolator());
-        set.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-              /*  selectedImage.setAlpha(1f);*/
-                expandedImageView.setVisibility(View.GONE);
-                mCurrentAnimator = null;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                selectedImage.setAlpha(1f);
-
-                expandedImageView.setVisibility(View.GONE);
-                mCurrentAnimator = null;
-            }
-        });
-        expandedImageView.reset();
-        set.start();
-        mCurrentAnimator = set;
+        toolbar.setTitle(R.string.add_point);
+        updateMenuItem("Adicionar", R.drawable.ic_check);
+        expandedImageView.setVisibility(View.GONE);
+        expandedImageView.setDisplayMatrix(new Matrix());
+        expandedImageView.setSuppMatrix(new Matrix());
         imageOpen = false;
+    }
+
+    private void updateMenuItem(String title, int icon) {
+        MenuItem item = menu.findItem(R.id.action_check);
+        if (item != null) {
+            item.setTitle(title);
+            item.setIcon(icon);
+        }
+
     }
 
 
